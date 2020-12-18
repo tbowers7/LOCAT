7bdabf4de6330985b84f4df44daba08efe063313
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2020-12-18 12:44:08 -0700

    Add init, setup, and requirements files to repo.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of LDTaddCAT.
+#
+#   This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Created on 18-Dec-2020
+#
+#  @author: tbowers
+
+from .parse_gaia import *
